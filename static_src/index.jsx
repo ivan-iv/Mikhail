a45487643e3499c1f0d5f5ab316e59347a196978
ivan <ii2357652@gmail.com>
2020-04-28 23:23:47 +0300

    Домашнее задание №1

---
+++ static_src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react' ;
+import ReactDOM from 'react-dom' ;
+let messages = [ 'Привет' , 'Как дела?' ] ;
+const MessageComponent = (props) => <div> {props. text } </div> ;
+const MessageField = (props) => {
+    return props. messages . map (message => <MessageComponent text = { message } /> ) ;
+} ;
+ReactDOM. render (
+<MessageField messages = { messages } /> ,
+document. getElementById ( 'root' ) ,
+) ;
